6b5457281f51e0a08c24382cc6a85eb825905b9a
lu8336787856 <lu306988010@163.com>
2016-04-11 21:11:40 +0800

    第一次提交

---
+++ WXMS/Model/MessageHelp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// 消息处理类
+    /// </summary>
+    public class MessageHelp
+    {
+        private static MessageHelp instance = null;
+
+        /// <summary>
+        /// 单例
+        /// </summary>
+        /// <returns></returns>
+        public static MessageHelp getInstance() 
+        {
+            if (instance == null) 
+            {
+                instance = new MessageHelp();
+            }
+            return instance;
+        }
+
+        /// <summary>
+        /// 统一处理消息接口
+        /// </summary>
+        /// <param name="postStr">消息的内容 Xml格式</param>
+        /// <returns></returns>
+        public string HandleMessage(string postStr) 
+        {
+
+            return "";
+        }
+    }
+}
